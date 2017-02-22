201c77536fc5727d844c39202d0ab972018aa6c4
Oluwafemi Sule <oluwafemisule@outlook.com>
2017-02-22 14:47:49 +0100

    check if a number is perfect

--- tail.erl
+++ tail.erl
@@ -1,5 +1,5 @@
 -module(tail).
--export([fib/1, fac/1]).
+-export([fib/1, fac/1, perfect/1]).
 
 % P is previous value
 % C  is current value 
@@ -19,4 +19,16 @@ fac(N) ->
 fac(0, P) ->
   P;
 fac(N, P) ->
-  fac(N-1, N*P).
+fac(N-1, N*P).
+
+% counting up to N, S is the sum of divisors
+% a perfect number is one whose sum of divisors adds up to it.
+perfect(N, N, S) ->
+  N == S;
+perfect(N, M, S)  when N rem M == 0 ->
+  perfect(N, M+1, S + M);
+perfect(N, M, S) ->
+  perfect(N, M+1, S).
+
+perfect(N) ->
+  perfect(N, 1, 0).
