c0d239094636177c327604af624aaaa3ed4276a0
Oluwafemi Sule <oluwafemisule@outlook.com>
2017-03-02 14:16:04 +0100

    define take using a builtin function

--- week2/ibegin.erl
+++ week2/ibegin.erl
@@ -1,5 +1,5 @@
 -module(ibegin).
--export([take/2]).
+-export([take/2, take2/2, take3/2]).
 
 % Specification of the type of function
 -spec take(integer(), [T]) -> [T].
@@ -11,3 +11,18 @@ take(_N, []) ->
   [];
 take(N, [X|Xs]) when N>0 ->
   [X|take(N-1, Xs)].
+
+% re-use split from lists.erl
+take2(N, Xs) ->
+  {Front, _Back} = lists:split(N, Xs),
+  Front.
+
+% modify definition of split in lists.erl to make a take
+take3(N, List) ->
+  take3(N, List, []).
+take3(0, _L, R) ->
+  lists:reverse(R, []);
+take3(N, [H|T], R) ->
+  take3(N-1, T, [H|R]);
+take3(_, [], R) ->
+  lists:reverse(R, []).
